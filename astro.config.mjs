b1d348df753617576e996be47784712cd687cbe3
Michael Salazar <michael@standardpaintprotection.com>
2026-06-05 12:44:23 -1000

    add botanical favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://pakalolo.garden',
+  vite: {
+    server: {
+      fs: {
+        allow: ['..']
+      }
+    }
+  }
 });
