779b41969168534069ca0185ca05199b29ca1b30
Michael Salazar <michael@standardpaintprotection.com>
2026-06-05 19:16:24 -1000

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://pakalolo.garden',
+
   vite: {
     server: {
       fs: {
         allow: ['..']
       }
     }
-  }
+  },
+
+  integrations: [sitemap()]
 });
